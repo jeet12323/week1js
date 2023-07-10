31645b9f3f7cf519fc37b24391858923d1788bab
jeet12323 <21it043@charusat.edu.in>
2023-07-10 21:12:32 +0530

    first commit

---
+++ task2.js
@@ -0,0 +1,11 @@
+function calculateOperations(num1, num2) {
+    var sum = num1 + num2; var difference =
+    num1 - num2; var product = num1 * num2;
+    var quotient = num1 / num2;
+    return {
+    sum: sum,
+    difference: difference,
+    product: product,
+    quotient: quotient
+    };
+    }
